16cc46db77364ecb2db06f186f70bcb27d47e016
Eugene Bogorad <bogorad@gmail.com>
2025-09-02 10:38:05 +0200

    flake - current nodejs

--- flake.nix
+++ flake.nix
@@ -1,25 +1,45 @@
 {
-  description = "Node.js development environment";
+  description = "Run Africa Worker Development Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_22
+          packages = with pkgs; [
+            nodejs_24
+            wrangler
+            nix-ld
+            zsh
             nodePackages.pnpm
             nodePackages.typescript
           ];
 
+          shell = "${pkgs.zsh}/bin/zsh";
+
           shellHook = ''
+            export NIX_LD_LIBRARY_PATH="${
+              pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+                pkgs.zlib
+                pkgs.openssl
+                pkgs.libgcc.lib
+              ]
+            }:$NIX_LD_LIBRARY_PATH"
+            export NIX_LD="${pkgs.stdenv.cc.cc.lib}/lib/ld-linux-x86-64.so.2"
             echo "Node.js development environment loaded"
             echo "Node.js version: $(node --version)"
             echo "pnpm version: $(pnpm --version)"
