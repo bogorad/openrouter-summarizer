387a086a11d7ec18cc81f19b2284919a1d877e00
Eugene Bogorad <bogorad@gmail.com>
2025-09-10 13:08:42 +0200

    no need for wrangler

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs_24
-            wrangler
             nix-ld
             zsh
             nodePackages.pnpm
@@ -29,22 +28,6 @@
           ];
 
           shell = "${pkgs.zsh}/bin/zsh";
-
-          shellHook = ''
-            export NIX_LD_LIBRARY_PATH="${
-              pkgs.lib.makeLibraryPath [
-                pkgs.stdenv.cc.cc.lib
-                pkgs.zlib
-                pkgs.openssl
-                pkgs.libgcc.lib
-              ]
-            }:$NIX_LD_LIBRARY_PATH"
-            export NIX_LD="${pkgs.stdenv.cc.cc.lib}/lib/ld-linux-x86-64.so.2"
-            echo "Node.js development environment loaded"
-            echo "Node.js version: $(node --version)"
-            echo "pnpm version: $(pnpm --version)"
-            echo "TypeScript version: $(tsc --version)"
-          '';
         };
       }
     );
